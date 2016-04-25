e6a025b2e008ba0aff13cd738b7aa1d15cd21224
tunadao1 <hugorafaelmc@gmail.com>
2016-04-24 22:35:09 -0300

    Added files via upload

--- script.js
+++ script.js
@@ -4,8 +4,18 @@
     scotchApp.config(function($routeProvider) {
         $routeProvider
 
+            // route for the home page
+            .when('/', {
+                controller  : 'mainController'
+            })
+
             // route for the contact page
             .when('/ligar', {
                 templateUrl : 'pages/teste.html'
             });
+    });
+
+        // create the controller and inject Angular's $scope
+    scotchApp.controller('mainController', function($scope) {
+        alert("teste");
     });
