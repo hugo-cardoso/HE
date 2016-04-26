ef8c23ede8f5738e07b01966f07bc57bd4c9c538
tunadao1 <hugorafaelmc@gmail.com>
2016-04-25 21:30:31 -0300

    Added files via upload

--- script.js
+++ script.js
@@ -18,16 +18,56 @@
                 templateUrl : 'pages/tela_inicial.html'
             })
 
+            .when('/internet', {
+                templateUrl : 'pages/menu/internet.html'
+            })
+
+            .when('/resets', {
+                templateUrl : 'pages/menu/resets.html',
+                controller  : 'resetsController'
+            })
 
             // route for the contact page
             .when('/ligar', {
                 templateUrl : 'pages/teste.html'
             });
-    });
+        });
 
         // create the controller and inject Angular's $scope
-    scotchApp.controller('mainController', function($scope) {
-        
+        scotchApp.controller('mainController', function($scope) {
 
 
-    });
+
+        });
+
+        // create the controller and inject Angular's $scope
+        scotchApp.controller('resetsController', function($scope) {
+
+            $('select.dropdown')
+            .dropdown()
+            ;
+            
+            $("#resets").change(function(){
+
+            var input = $("#resets").val();
+
+                if(input === "5_0"){
+                    $("#5_0").show();
+                    $("#5_1").hide();
+                    $("#6_0").hide();
+                }
+                if(input === "5_1"){
+                    $("#5_0").hide();
+                    $("#5_1").show();
+                    $("#6_0").hide();
+                }
+                if(input === "6_0"){
+                    $("#5_0").hide();
+                    $("#5_1").hide();
+                    $("#6_0").show();
+                }
+
+
+            })
+
+        });
